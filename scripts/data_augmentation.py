6de6a08fddb223140f40545ac17021c9c9903720
jsdysw <jsdysw@gmail.com>
2023-12-15 02:09:32 +0000

    refactor - remove magic numbers

--- scripts/data_augmentation.py
+++ scripts/data_augmentation.py
@@ -3,17 +3,17 @@ from tqdm.auto import tqdm
 import random
 import pandas as pd
 
-def data_augmentation(path, ratio=1):
+def data_augmentation(path, num_of_samples=1):
     data = pd.read_csv(path)
 
     aug_rows = []
     for src_idx, src_row in tqdm(data.iterrows(), desc='data_augmentation', total=len(data)):
         aug_rows.append([f'{src_idx}_{src_idx}', 'augmented', src_row['sentence_2'], src_row['sentence_1'], src_row['label'], src_row['binary-label']])
-        for trg_idx, trg_row in data.sample(n=1).iterrows():
+        for trg_idx, trg_row in data.sample(n=num_of_samples).iterrows():
             if src_idx == trg_idx:
                 continue
-            aug_rows.append([f'{src_idx}_{trg_idx}', 'augmented', src_row['sentence_1'], trg_row['sentence_2'], 0.3, 0])
-            # aug_rows.append(['augmented', trg_row['sentence_2'], src_row['sentence_1'], 0.0, 0])
+            aug_rows.append([f'{src_idx}_{trg_idx}', 'augmented', src_row['sentence_1'], trg_row['sentence_2'], random.uniform(0.0, 1.0), 0])
+    
     aug_df = pd.DataFrame(aug_rows, columns=data.columns) 
     data = pd.concat([data, aug_df])
     print('num of augmented data', len(aug_rows))
