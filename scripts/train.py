2e429ae6ab648c4c5159e995921e77dfaade08d4
hyemin <hanhyemin1130@gmail.com>
2023-12-16 01:11:49 +0900

    change learning rate

--- scripts/train.py
+++ scripts/train.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--max_epoch', default=2, type=int)
     parser.add_argument('--shuffle', default=True)
-    parser.add_argument('--learning_rate', default=1e-5, type=float)
+    parser.add_argument('--learning_rate', default=1e-4, type=float)
     parser.add_argument('--train_path', default='../../data/train.csv')
     parser.add_argument('--dev_path', default='../../data/dev.csv')
     parser.add_argument('--test_path', default='../../data/dev.csv')
