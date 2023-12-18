af4263b647ccd2b365d9304254b7241211df064f
jsdysw <jsdysw@gmail.com>
2023-12-18 05:37:22 +0000

    fix - ensemble models with different configs

--- inference.py
+++ inference.py
@@ -14,9 +14,7 @@ from model.model import Model
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', default='monologg/koelectra-base-v3-discriminator', type=str)
     parser.add_argument('--batch_size', default=16, type=int)
-    parser.add_argument('--max_epoch', default=1, type=int)
     parser.add_argument('--shuffle', default=True)
     parser.add_argument('--train_path', default='../data/train.csv')
     parser.add_argument('--dev_path', default='../data/dev.csv')
@@ -24,30 +22,32 @@ if __name__ == '__main__':
     parser.add_argument('--predict_path', default='../data/test.csv')
     args = parser.parse_args(args=[])
 
-    # dataloader와 model을 생성합니다.
-    dataloader = Dataloader(args.model_name, args.batch_size, args.shuffle, args.train_path, args.dev_path,
-                            args.test_path, args.predict_path)
-
-    trainer = pl.Trainer(accelerator="gpu", devices=1, max_epochs=args.max_epoch, log_every_n_steps=1)
-
-    # Inference part
-    models_path = [
-        '/data/ephemeral/home/level1-semantictextsimilarity-nlp-12/lightning_logs/version_36/checkpoints/sts-epoch=14-val_pearson=0.92.ckpt',
-        '/data/ephemeral/home/level1-semantictextsimilarity-nlp-12/lightning_logs/version_50/checkpoints/sts-epoch=43-val_pearson=0.92.ckpt'
+    models_config = [
+        {
+            'path':'/data/ephemeral/home/level1-semantictextsimilarity-nlp-12/lightning_logs/version_36/checkpoints/sts-epoch=14-val_pearson=0.92.ckpt',
+            'model_name':'monologg/koelectra-base-v3-discriminator'
+        },
+        {
+            'path':'/data/ephemeral/home/level1-semantictextsimilarity-nlp-12/lightning_logs/version_50/checkpoints/sts-epoch=43-val_pearson=0.92.ckpt',
+            'model_name':'monologg/koelectra-base-v3-discriminator'
+        }
     ]
     OUT_PATH = './output_ensemble.csv'
 
     predictions_list = []
-    for path in models_path:
-        model = Model.load_from_checkpoint(path)
-        model.eval()
+    for config in models_config:
+        dataloader = Dataloader(config['model_name'], args.batch_size, args.shuffle, args.train_path, args.dev_path,
+                            args.test_path, args.predict_path)
+        trainer = pl.Trainer(accelerator="gpu", devices=1, max_epochs=1, log_every_n_steps=1)
+
+        model = Model.load_from_checkpoint(config['path'])
         trainer.test(model=model, datamodule=dataloader)
 
         predictions = trainer.predict(model=model, datamodule=dataloader)
         predictions = list(round(float(i), 1) for i in torch.cat(predictions))
         predictions_list.append(predictions)
 
-    predictions_final = [round(sum(x)/len(models_path),1) for x in zip(*predictions_list)]
+    predictions_final = [round(sum(x)/len(models_config),1) for x in zip(*predictions_list)]
     
     # output 형식을 불러와서 예측된 결과로 바꿔주고, output.csv로 출력합니다.
     output = pd.read_csv('../data/sample_submission.csv')
