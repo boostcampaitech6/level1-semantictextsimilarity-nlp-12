7211ca177798d7f093f0cc1cb7eac1b5a049f392
hyemin <hanhyemin1130@gmail.com>
2023-12-18 22:23:44 +0900

    refactor - change csvfile

--- train.py
+++ train.py
@@ -22,14 +22,14 @@ if __name__ == '__main__':
     # 실행 시 '--batch_size=64' 같은 인자를 입력하지 않으면 default 값이 기본으로 실행됩니다
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', default='monologg/koelectra-base-v3-discriminator', type=str)
-    parser.add_argument('--batch_size', default=64, type=int)
+    parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--max_epoch', default=5, type=int)
     parser.add_argument('--shuffle', default=True)
     parser.add_argument('--learning_rate', default=1e-5, type=float)
-    parser.add_argument('--train_path', default='../data/train.csv')
-    parser.add_argument('--dev_path', default='../data/dev.csv')
-    parser.add_argument('--test_path', default='../data/dev.csv')
-    parser.add_argument('--predict_path', default='../data/test.csv')
+    parser.add_argument('--train_path', default='../data/train_cleaning_2.csv')
+    parser.add_argument('--dev_path', default='../data/dev_cleaning_2.csv')
+    parser.add_argument('--test_path', default='../data/dev_cleaning_2.csv')
+    parser.add_argument('--predict_path', default='../data/test_cleaning_2.csv')
     args = parser.parse_args(args=[])
 
     # dataloader와 model을 생성합니다.
@@ -37,20 +37,20 @@ if __name__ == '__main__':
                             args.test_path, args.predict_path)
     model = Model(args.model_name, args.learning_rate)
         
-    # checkpoint_callback = ModelCheckpoint(
-    #     every_n_train_steps=1,
-    #     save_top_k=1,
-    #     monitor="val_pearson",      #'val_loss'
-    #     mode='max',                 #'min'
-    #     filename="sts-{epoch:02d}-{val_pearson:.2f}",
-    # )
+    checkpoint_callback = ModelCheckpoint(
+        every_n_train_steps=1,
+        save_top_k=1,
+        monitor="val_pearson",      #'val_loss'
+        mode='max',                 #'min'
+        filename="sts-{epoch:02d}-{val_pearson:.2f}",
+    )
 
     # gpu가 없으면 accelerator="cpu"로 변경해주세요, gpu가 여러개면 'devices=4'처럼 사용하실 gpu의 개수를 입력해주세요
     trainer = pl.Trainer(
         accelerator="gpu", 
         devices=1, 
         max_epochs=args.max_epoch, 
-        # callbacks=[checkpoint_callback],
+        callbacks=[checkpoint_callback],
         log_every_n_steps=1
     )
 
@@ -58,5 +58,5 @@ if __name__ == '__main__':
     trainer.fit(model=model, datamodule=dataloader)
     trainer.test(model=model, datamodule=dataloader)
 
-    # 학습이 완료된 모델을 저장합니다.
-    torch.save(model, '../model_dir/sts-{epoch:02d}-{val_pearson:.2f}-model.pt')
+    # # 학습이 완료된 모델을 저장합니다.
+    # torch.save(model, f'../model_dir/sts-{args.model_name}-32-100-model.pt')
