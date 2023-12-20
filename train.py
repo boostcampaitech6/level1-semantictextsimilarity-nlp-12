cca3439be9fc01c276ec120618a92e6af00034fd
jsdysw <jsdysw@gmail.com>
2023-12-20 12:29:39 +0000

    feat - train config

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ import random
 import torch
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+import yaml
 
 from data_loaders.data_loaders import Dataloader
 from model.model import Model
@@ -16,29 +18,23 @@ random.seed(0)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', default='monologg/koelectra-base-v3-discriminator', type=str)
-    # parser.add_argument('--model_name', default='snunlp/KR-SBERT-V40K-klueNLI-augSTS', type=str)
-    parser.add_argument('--batch_size', default=512, type=int)
-    parser.add_argument('--max_epoch', default=50, type=int)
-    parser.add_argument('--shuffle', default=True)
-    parser.add_argument('--learning_rate', default=1e-5, type=float)
-    parser.add_argument('--train_path', default='./train_augmented_4.csv')
-    parser.add_argument('--dev_path', default='../data/dev.csv')
-    parser.add_argument('--test_path', default='../data/dev.csv')
-    parser.add_argument('--predict_path', default='../data/test.csv')
+    parser.add_argument('--use_sweep', default=False, type=bool)
+    parser.add_argument('--wandb_logger', default=False, type=bool)
     parser.add_argument('--fine_tuning', default=False, type=bool)
+    parser.add_argument('--config', default='./configs/xlm-roberta-large.yml', type=str)
     args = parser.parse_args(args=[])
 
+    with open(args.config, "r") as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+
     # dataloader와 model을 생성합니다.
-    dataloader = Dataloader(args.model_name, args.batch_size, args.shuffle, args.train_path, args.dev_path,
-                            args.test_path, args.predict_path)
+    dataloader = Dataloader(cfg['model_name'], cfg['batch_size'], cfg['shuffle'], cfg['train_path'], 
+                            cfg['dev_path'],cfg['test_path'], cfg['predict_path'], cfg['max_sentence_length'])
 
     if args.fine_tuning:
-        checkpoint_name = 'sts-epoch=19-val_pearson=0.79'
-        PATH = f'/data/ephemeral/home/level1-semantictextsimilarity-nlp-12/lightning_logs/version_40/checkpoints/{checkpoint_name}.ckpt'
-        model = Model.load_from_checkpoint(PATH)
+        model = Model.load_from_checkpoint(cfg['ckpt_path'])
     else:
-        model = Model(args.model_name, args.learning_rate)
+        model = Model(cfg['model_name'], cfg['learning_rate'])
     print(model)
 
     checkpoint_callback = ModelCheckpoint(
@@ -46,16 +42,26 @@ if __name__ == '__main__':
         save_top_k=1,
         monitor="val_pearson",      #'val_loss' val_pearson
         mode='max',                 #'min' max
-        filename="sts-{epoch:02d}-{val_pearson:.2f}",
+        filename="sts-{epoch:02d}-{val_pearson:.3f}",
     )
 
-    trainer = pl.Trainer(
-        accelerator="gpu", 
-        devices=1, 
-        max_epochs=args.max_epoch, 
-        callbacks=[checkpoint_callback],
-        log_every_n_steps=1
-    )
+    if args.wandb_logger:
+        trainer = pl.Trainer(
+            accelerator=cfg['accelerator'], 
+            devices=1, 
+            max_epochs=cfg['max_epoch'], 
+            callbacks=[checkpoint_callback],
+            log_every_n_steps=1,
+            logger=WandbLogger(project=f"sts-{cfg['model_name']}")
+        )
+    else:
+        trainer = pl.Trainer(
+            accelerator=cfg['accelerator'], 
+            devices=1, 
+            max_epochs=cfg['max_epoch'], 
+            callbacks=[checkpoint_callback],
+            log_every_n_steps=1,
+        )
 
     # Train part
     trainer.fit(model=model, datamodule=dataloader)
