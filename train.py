9929a18b87a3751deefeddc0008ab3909b05d2d8
hyemin <hanhyemin1130@gmail.com>
2023-12-20 11:17:31 +0900

    feat - prepare to use wandb

--- train.py
+++ train.py
@@ -8,6 +8,16 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from data_loaders.data_loaders import Dataloader
 from model.model import Model
 
+from pytorch_lightning.loggers import WandbLogger
+wandb_logger = WandbLogger(project='sts-origin_datasets',
+                           name = 'koelectra-base-v3-discriminator',
+                           log_model='True',
+                           save_dir = '../wandb')
+# 시간 기록
+import time
+from datetime import timedelta
+start_time = time.time()
+
 # seed 고정
 torch.manual_seed(0)
 torch.cuda.manual_seed(0)
@@ -16,9 +26,9 @@ random.seed(0)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # parser.add_argument('--model_name', default='monologg/koelectra-base-v3-discriminator', type=str)
+    # parser.add_argument('--model_name', default='xlm-roberta-large', type=str)
+    parser.add_argument('--model_name', default='monologg/koelectra-base-v3-discriminator', type=str)
     # parser.add_argument('--model_name', default='snunlp/KR-ELECTRA-discriminator', type=str)
-    parser.add_argument('--model_name', default='xlm-roberta-large', type=str)
     parser.add_argument('--batch_size', default=64, type=int)
     parser.add_argument('--max_epoch', default=50, type=int)
     parser.add_argument('--shuffle', default=True)
@@ -35,8 +45,8 @@ if __name__ == '__main__':
                             args.test_path, args.predict_path)
 
     if args.fine_tuning:
-        checkpoint_name = 'sts-epoch=19-val_pearson=0.79'
-        PATH = f'/data/ephemeral/home/level1-semantictextsimilarity-nlp-12/lightning_logs/version_40/checkpoints/{checkpoint_name}.ckpt'
+        checkpoint_name = 'sts-epoch=33-val_pearson=0.917'
+        PATH = f'/data/ephemeral/home/code/lightning_logs/version_2/checkpoints/{checkpoint_name}.ckpt'
         model = Model.load_from_checkpoint(PATH)
     else:
         model = Model(args.model_name, args.learning_rate)
@@ -47,7 +57,7 @@ if __name__ == '__main__':
         save_top_k=1,
         monitor="val_pearson",      #'val_loss' val_pearson
         mode='max',                 #'min' max
-        filename="sts-{epoch:02d}-{val_pearson:.3f}",
+        filename="sts-origin_dataset-{epoch:02d}-{val_pearson:.3f}",
     )
 
     trainer = pl.Trainer(
@@ -55,9 +65,15 @@ if __name__ == '__main__':
         devices=1, 
         max_epochs=args.max_epoch, 
         callbacks=[checkpoint_callback],
-        log_every_n_steps=1
+        log_every_n_steps=1,
+        # logger=wandb_logger # WandB 추가
     )
 
     # Train part
     trainer.fit(model=model, datamodule=dataloader)
     trainer.test(model=model, datamodule=dataloader)
+
+end_time = time.time()
+execution_time = end_time - start_time
+execution_timedelta = timedelta(seconds=execution_time)
+print(f"코드 실행 시간: {execution_timedelta}")
